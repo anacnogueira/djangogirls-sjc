96916abf0f9ca42951fcd7fbfe7b26e2ca7d8e34
Ana Claudia <anacnogueira@gmail.com>
2017-05-14 23:10:04 -0300

    My DFjango GIrls app, first commit

---
+++ python_intro.py
@@ -0,0 +1,12 @@
+def hi(name):
+	print('Hello '+ name + '!')
+
+girls = ['Ana','Raquel', 'Monica', 'Phoebe', 'Ola', 'You']
+
+for name in girls:
+	hi(name)
+	print('Next girl')
+
+for i in range(1,6):
+	print(i)	
+
